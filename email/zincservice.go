3632bbd39dbf6ad812d16cdb16020693be789aa1
Alejandro Morales <a.moralesc@javeriana.edu.co>
2023-03-12 22:36:48 -0500

    Add custom index creation

--- email/zincservice.go
+++ email/zincservice.go
@@ -8,34 +8,107 @@ import (
 	"net/http"
 )
 
+type BulkEmails struct {
+	Index   string  `json:"index"`
+	Records []Email `json:"records"`
+}
+
 const (
-	uploadPath      = "/api/_bulkv2"
-	deleteIndexPath = "/api/index/emails"
+	uploadPath = "/api/_bulkv2"
+	indexPath  = "/api/index/"
 )
 
-func UploadEmails(bulk BulkEmails, zincUrl string, zincAdminUser string, zincAdminPassword string) error {
-	// Convert the struct to JSON
-	jsonBytes, err := json.Marshal(bulk)
-	if err != nil {
-		return err
+const emailsIndexMapping = `
+{
+	"name": "emails",
+	"storage_type": "disk",
+	"mappings": {
+		"properties": {
+			"message-id": {
+				"type": "keyword",
+				"index": true,
+				"store": false,
+				"sortable": false,
+				"aggregatable": false,
+				"highlightable": false
+			},
+			"date": {
+				"type": "date",
+				"format": "2006-01-02T15:04:05Z07:00",
+				"index": true,
+				"store": false,
+				"sortable": true,
+				"aggregatable": true,
+				"highlightable": false
+			},
+			"from": {
+				"type": "keyword",
+				"index": true,
+				"store": true,
+				"sortable": false,
+				"aggregatable": false,
+				"highlightable": false
+			},
+			"to": {
+				"type": "keyword",
+				"index": true,
+				"store": true,
+				"sortable": false,
+				"aggregatable": false,
+				"highlightable": false
+			},
+			"subject": {
+				"type": "text",
+				"index": true,
+				"store": false,
+				"sortable": false,
+				"aggregatable": false,
+				"highlightable": false
+			},
+			"cc": {
+				"type": "keyword",
+				"index": true,
+				"store": true,
+				"sortable": false,
+				"aggregatable": false,
+				"highlightable": false
+			},
+			"bcc": {
+				"type": "keyword",
+				"index": true,
+				"store": true,
+				"sortable": false,
+				"aggregatable": false,
+				"highlightable": false
+			},
+			"body": {
+				"type": "text",
+				"index": true,
+				"store": false,
+				"sortable": false,
+				"aggregatable": false,
+				"highlightable": false
+			}
+		}
 	}
-	jsonReader := bytes.NewReader(jsonBytes)
+}`
 
-	// Create the post request
-	req, err := http.NewRequest("POST", zincUrl+uploadPath, jsonReader)
+func CreateIndex(zincUrl string, zincAdminUser string, zincAdminPassword string) error {
+	// create the post request
+	req, err := http.NewRequest("POST", zincUrl+indexPath, bytes.NewReader([]byte(emailsIndexMapping)))
 	if err != nil {
 		return err
 	}
 	req.SetBasicAuth(zincAdminUser, zincAdminPassword)
 	req.Header.Set("Content-Type", "application/json")
 
-	// Send the request
+	// send the request
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
 
+	// check the response
 	body, _ := io.ReadAll(resp.Body)
 	if resp.StatusCode != 200 {
 		return fmt.Errorf("zinc server responded with code %v: %v", resp.StatusCode, string(body))
@@ -44,20 +117,52 @@ func UploadEmails(bulk BulkEmails, zincUrl string, zincAdminUser string, zincAdm
 	return nil
 }
 
-func DeleteIndex(zincUrl string, zincAdminUser string, zincAdminPassword string) error {
-	// Create the delete request
-	req, err := http.NewRequest("DELETE", zincUrl+deleteIndexPath, nil)
+func UploadEmails(bulk BulkEmails, zincUrl string, zincAdminUser string, zincAdminPassword string) error {
+	// convert the struct to JSON
+	jsonBytes, err := json.Marshal(bulk)
+	if err != nil {
+		return err
+	}
+
+	// create the post request
+	req, err := http.NewRequest("POST", zincUrl+uploadPath, bytes.NewReader(jsonBytes))
+	if err != nil {
+		return err
+	}
+	req.SetBasicAuth(zincAdminUser, zincAdminPassword)
+	req.Header.Set("Content-Type", "application/json")
+
+	// send the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// check the response
+	body, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("zinc server responded with code %v: %v", resp.StatusCode, string(body))
+	}
+
+	return nil
+}
+
+func DeleteIndex(indexName string, zincUrl string, zincAdminUser string, zincAdminPassword string) error {
+	// create the delete request
+	req, err := http.NewRequest("DELETE", zincUrl+indexPath+indexName, nil)
 	if err != nil {
 		return err
 	}
 	req.SetBasicAuth(zincAdminUser, zincAdminPassword)
 
-	// Send the request
+	// send the request
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
 
+	// check the response
 	body, _ := io.ReadAll(resp.Body)
 	if resp.StatusCode != 200 {
 		return fmt.Errorf("zinc server responded with code %v: %v", resp.StatusCode, string(body))
--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 	"github.com/amoralesc/indexer/utils"
 )
 
-var NumParserWorkers, _ = strconv.Atoi(utils.GetenvOrDefault("NUM_PARSER_WORKERS", "8"))
 var NumUploaderWorkers, _ = strconv.Atoi(utils.GetenvOrDefault("NUM_UPLOADER_WORKERS", "4"))
+var NumParserWorkers, _ = strconv.Atoi(utils.GetenvOrDefault("NUM_PARSER_WORKERS", "8"))
 var BulkUploadSize, _ = strconv.Atoi(utils.GetenvOrDefault("BULK_UPLOAD_SIZE", "5000"))
 var ZincUrl = fmt.Sprintf("http://localhost:%v", utils.GetenvOrDefault("ZINC_PORT", "4080"))
 var ZincAdminUser = utils.GetenvOrDefault("ZINC_ADMIN_USER", "admin")
@@ -24,12 +24,20 @@ func main() {
 	dir := flag.String("d", "", "Directory of the emails. If none is provided, the server will use already indexed emails.")
 	flag.Parse()
 
+	// only parse and upload emails if a directory is provided
 	if *dir != "" {
 		log.Printf("INFO: deleting emails index (if exists)")
-		err := email.DeleteIndex(ZincUrl, ZincAdminUser, ZincAdminPassword)
+		err := email.DeleteIndex("emails", ZincUrl, ZincAdminUser, ZincAdminPassword)
 		if err != nil {
 			log.Println("WARNING: failed to delete emails index: ", err)
 		}
+
+		log.Printf("INFO: creating emails index")
+		err = email.CreateIndex(ZincUrl, ZincAdminUser, ZincAdminPassword)
+		if err != nil {
+			log.Fatal("FATAL: failed to create emails index: ", err)
+		}
+
 		log.Println("INFO: starting to parse and upload emails")
 		start := time.Now()
 		routines.ParseAndUploadEmails(dir, NumUploaderWorkers, NumParserWorkers, BulkUploadSize, ZincUrl, ZincAdminUser, ZincAdminPassword)
