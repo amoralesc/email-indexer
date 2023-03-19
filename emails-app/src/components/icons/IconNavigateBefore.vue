712ae80aacb3da96e0f4065ed0798e9b4f0b24b3
Alejandro Morales <a.moralesc@javeriana.edu.co>
2023-03-18 21:02:12 -0500

    Change logo, add icons

---
+++ emails-app/src/components/icons/IconNavigateBefore.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="currentColor">
+    <path d="M14 17.65 8.35 12 14 6.35l1.05 1.05-4.6 4.6 4.6 4.6Z" />
+  </svg>
+</template>
