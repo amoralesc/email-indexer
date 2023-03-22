78d058f147e138483b8a34485fb50f218a8ac6e3
Alejandro Morales <a.moralesc@javeriana.edu.co>
2023-03-21 23:45:40 -0500

    Fix icons viewport

--- emails-app/src/components/icons/IconNavigateBefore.vue
+++ emails-app/src/components/icons/IconNavigateBefore.vue
@@ -1,5 +1,11 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="currentColor">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    width="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
     <path d="M14 17.65 8.35 12 14 6.35l1.05 1.05-4.6 4.6 4.6 4.6Z" />
   </svg>
 </template>
--- emails-app/src/components/icons/IconTune.vue
+++ emails-app/src/components/icons/IconTune.vue
@@ -1,6 +1,13 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="currentColor">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    width="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
     <path
-      d="M11.25 20.75v-5.5h1.5v2h8v1.5h-8v2Zm-8-2v-1.5h5.5v1.5Zm4-4v-2h-4v-1.5h4v-2h1.5v5.5Zm4-2v-1.5h9.5v1.5Zm4-4v-5.5h1.5v2h4v1.5h-4v2Zm-12-2v-1.5h9.5v1.5Z" />
+      d="M11.25 20.75v-5.5h1.5v2h8v1.5h-8v2Zm-8-2v-1.5h5.5v1.5Zm4-4v-2h-4v-1.5h4v-2h1.5v5.5Zm4-2v-1.5h9.5v1.5Zm4-4v-5.5h1.5v2h4v1.5h-4v2Zm-12-2v-1.5h9.5v1.5Z"
+    />
   </svg>
 </template>
